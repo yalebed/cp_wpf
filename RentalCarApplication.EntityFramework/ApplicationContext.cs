a211de6508beea796d1a16d9975d793eae593df1
Polina Lebed <lebedzpolina@gmail.com>
2026-04-16 02:04:15 +0300

    первый залив проекта

---
+++ RentalCarApplication.EntityFramework/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalCarApplication.Core.Model;
+using RentalCarApplication.EntityFramework.Configurations;
+
+namespace RentalCarApplication.EntityFramework
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public ApplicationContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost; Database=RentalCarDB; Trusted_Connection=True; TrustServerCertificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CarConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+        }
+    }
+}
